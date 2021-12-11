176e0d2b92ffd64b5c573d6c06c104b41409d6ff
vnrtumu <vnr.tumu@gmail.com>
2021-12-11 20:33:53 +0530

    Initial Commit

---
+++ NetflixAppVenky/NetflixAppVenkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixAppVenkyApp.swift
+//  NetflixAppVenky
+//
+//  Created by APPLE on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct NetflixAppVenkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
